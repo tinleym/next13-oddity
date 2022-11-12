ff08229305dc9e2df4eafd6acfec2d0abee23e2b
mt <tinleym@gmail.com>
2022-11-12 13:34:37 -0800

    clarification

--- app/client.tsx
+++ app/client.tsx
@@ -4,14 +4,13 @@ const IS_SERVER = typeof document === typeof undefined;
 
 function ExampleClient() {
   if (IS_SERVER) {
-    console.log("IS SERVER");
+    console.log("IS SERVER (NO DOCUMENT)");
     import("next/headers").then((headers) => {
       console.log("server cookies fn", headers.cookies);
       console.log("server cookies fn result", headers.cookies());
     });
   } else {
-    console.log("IS CLIENT");
-    console.log("client cookies", document.cookie);
+    console.log("IS CLIENT (HAS DOCUMENT)");
   }
 
   return <div>nothing to see here; check server console for error</div>;
